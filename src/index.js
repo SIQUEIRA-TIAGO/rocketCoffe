b8ea4bec8588f5f1332a91238b65d29078a91211
Tiago Oliveira <tiago.oliveira@dataunique.com.br>
2023-04-02 20:18:27 -0300

    First Commit

---
+++ src/index.js
@@ -0,0 +1,4 @@
+document.getElementById('menu-open').addEventListener('click', () => {
+    const mobileNav = document.getElementsByClassName('mobile-nav')[0]
+    mobileNav.classList.toggle('open')
+})
